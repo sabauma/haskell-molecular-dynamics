d328510d6e333a3163c73809048a2a4d959eb0fe
Spenser Bauman <sabauma@gmail.com>
2014-11-25 00:01:38 -0500

    House keeping

--- src/MolecularDynamics/BHTree.hs
+++ src/MolecularDynamics/BHTree.hs
@@ -1,15 +1,20 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE BangPatterns    #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns    #-}
 module Main where
 
-import           Data.Vector.Unboxed      (Vector)
-import qualified Data.Vector.Unboxed      as V
-import           MolecularDynamics.System
+import           Control.Applicative
+import           Control.DeepSeq
+import           Control.Exception           (evaluate)
+import           Control.Monad               (void)
+import           Data.Time.Clock
+import           Data.Vector.Unboxed         (Unbox, Vector)
+import qualified Data.Vector.Unboxed         as V
 import           MolecularDynamics.Vec3
 
 -- A BHNode consists of a centers, the extent of the node in each direction, and
 -- all the subtrees.
--- The `size` field contains the aggregate "size" of all the objects contained
+-- The `mass` field contains the aggregate "mass" of all the objects contained
 -- in the node. For a gravitational simulation, that size would be the aggregate
 -- mass of the objects, but it could also be charge or some other physical
 -- propery.
@@ -20,6 +25,9 @@ data BHNode = BHNode
   , subtrees :: ![BHNode]
   } deriving (Show)
 
+instance NFData BHNode where
+  rnf BHNode{..} = rnf subtrees
+
 data Centroid = Centroid {-# UNPACK #-} !Vec3 {-# UNPACK #-} !Double
   deriving Show
 
@@ -28,14 +36,22 @@ data BoundingBox = BoundingBox {-# UNPACK #-} !Vec3 {-# UNPACK #-} !Vec3
 
 type PositionAndMass = (Vec3, Double)
 
+projL :: (Unbox a, Unbox b) => Vector (a, b) -> Vector a
+projL = V.map fst
+{-# INLINE projL #-}
+
+projR :: (Unbox a, Unbox b) => Vector (a, b) -> Vector b
+projR = V.map snd
+{-# INLINE projR #-}
+
 leaves :: BHNode -> Int
 leaves (subtrees -> s)
   | null s    = 1
   | otherwise = sum $ map leaves s
 
 boxCenter, boxSpan :: BoundingBox -> Vec3
-boxCenter (BoundingBox min max) = (max ^+^ min) ^* 0.5
-boxSpan (BoundingBox min max)   = max ^-^ min
+boxCenter (BoundingBox l r) = (r ^+^ l) ^* 0.5
+boxSpan (BoundingBox l r)   = r ^-^ l
 {-# INLINE boxCenter #-}
 {-# INLINE boxSpan   #-}
 
@@ -54,13 +70,15 @@ dup x = (x, x)
 -- God willing, all these operations will fuse
 computeBounds :: Vector Vec3 -> BoundingBox
 computeBounds = uncurry BoundingBox . V.foldl1' f . V.map dup
-  where f (!a, !b) (!l, !r) = (vectorMin a l, vectorMax b r)
+  where
+    f (!a, !b) (!l, !r) = (vectorMin a l, vectorMax b r)
 {-# INLINE computeBounds #-}
 
 computeCenter :: Vector PositionAndMass -> Centroid
 computeCenter particles = Centroid (mid ^/ mass) mass
-  where (mid, mass)     = V.foldl' f (zeroV, 0) particles
-        f (!l, !r) (!p, !m) = (l ^+^ m *^ p, r + m)
+  where
+    (!mid, !mass)       = V.foldl' f (zeroV, 0) particles
+    f (!l, !r) (!p, !m) = (l ^+^ m *^ p, r + m)
 {-# INLINE computeCenter #-}
 
 -- A function taking a bounding box and a position vector and producing an index
@@ -80,13 +98,21 @@ subBoxes (BoundingBox (Vec3 minx miny minz) (Vec3 maxx maxy maxz)) (Vec3 cx cy c
   , (yl, yr) <- [(miny, cy), (cy, maxy)]
   , (xl, xr) <- [(minx, cx), (cx, maxx)] ]
 {-# INLINE subBoxes #-}
+  {-[ BoundingBox (Vec3 minx miny minz) (Vec3 cx   cy   cz)-}
+  {-, BoundingBox (Vec3 cx   miny minz) (Vec3 maxx cy   cz)-}
+  {-, BoundingBox (Vec3 minx cy   minz) (Vec3 cx   maxy cz)-}
+  {-, BoundingBox (Vec3 cx   cy   minz) (Vec3 maxx maxy cz)-}
+  {-, BoundingBox (Vec3 minx miny cz) (Vec3 cx   cy   maxz)-}
+  {-, BoundingBox (Vec3 cx   miny cz) (Vec3 maxx cy   maxz)-}
+  {-, BoundingBox (Vec3 minx cy   cz) (Vec3 cx   maxy maxz)-}
+  {-, BoundingBox (Vec3 cx   cy   cz) (Vec3 maxx maxy maxz) ]-}
 
 -- It would be nice if this could be done in a single pass, but this is simple.
 partitionParticles :: Vector PositionAndMass -> Vector Int -> [Vector PositionAndMass]
 partitionParticles ps idx = [f 0, f 1, f 2, f 3, f 4, f 5, f 6, f 7]
   where
     ps' = V.zip ps idx
-    f i = V.map fst $ V.filter ((== i) . snd) ps'
+    f i = projL $ V.filter ((== i) . snd) ps'
 {-# INLINE partitionParticles #-}
 
 splitPoints :: BoundingBox -> Vector PositionAndMass -> [(BoundingBox, Vector PositionAndMass)]
@@ -95,14 +121,14 @@ splitPoints box points
   | otherwise            = filter (not . V.null . snd) $ zip boxes points'
   where
     mid     = boxCenter box
-    idx     = V.map (cellIndex mid . fst) points
+    idx     = V.map (cellIndex mid) $ projL points
     boxes   = subBoxes box mid
     points' = partitionParticles points idx
 {-# INLINE splitPoints #-}
 
 -- Compute a Barnes-Hut tree using a vector of positions and masses.
-createBHTree :: BoundingBox -> Vector PositionAndMass -> BHNode
-createBHTree box ps
+createBHTreeWithBox :: BoundingBox -> Vector PositionAndMass -> BHNode
+createBHTreeWithBox box ps
   | V.length ps <= 1 = BHNode { com = com, mass = mass, extent = extent, subtrees = [] }
   | otherwise        = BHNode { com = com, mass = mass, extent = extent, subtrees = subtrees }
   where
@@ -110,7 +136,11 @@ createBHTree box ps
     subspaces         = splitPoints box ps
     Vec3 dx dy dz     = boxSpan box
     extent            = dx `min` dy `min` dz
-    subtrees          = map (uncurry createBHTree) subspaces
+    subtrees          = map (uncurry createBHTreeWithBox) subspaces
+
+createBHTree :: Vector PositionAndMass -> BHNode
+createBHTree = createBHTreeWithBox <$> computeBounds . projL <*> id
+{-# INLINE createBHTree #-}
 
 test :: Vector (Vec3, Double)
 test = V.fromList $ do
@@ -119,10 +149,16 @@ test = V.fromList $ do
   z <- [1 .. 100]
   return (Vec3 x y z, x + y + z)
 
-testTree = createBHTree (computeBounds $ V.map fst test) test
+testTree :: BHNode
+testTree = createBHTree test
 
 main :: IO ()
-main = print $ leaves testTree
+main = do
+  void $ evaluate test
+  t1 <- getCurrentTime
+  print $ leaves testTree
+  t2 <- getCurrentTime
+  print $ diffUTCTime t2 t1
 
 --test :: Vector (Vec3, Double)
 --test = V.fromList [(Vec3 1 1 1, 1), (Vec3 0 0 1, 2), (Vec3 0 0 0, 3), (Vec3 0.5 0.5 0.5, 4)]
--- src/MolecularDynamics/Vec3.hs
+++ src/MolecularDynamics/Vec3.hs
@@ -1,72 +1,43 @@
+{-# LANGUAGE BangPatterns               #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 module MolecularDynamics.Vec3
     ( Vec3 (..)
-    , module Data.AdditiveGroup
-    , module Data.AffineSpace
-    , module Data.VectorSpace
-    , oneV
-    , vec3
-    , vecMap
-    , getX
-    , getY
-    , getZ
+    , module M
     ) where
 
-import qualified Data.Vector.Generic
-import qualified Data.Vector.Generic.Mutable
 import           Data.Vector.Unboxed.Deriving
 
-import           Data.AdditiveGroup
-import           Data.AffineSpace
-import           Data.VectorSpace
+import           Data.AdditiveGroup as M
+import           Data.AffineSpace   as M
+import           Data.VectorSpace   as M
 
 data Vec3 = Vec3 {-# UNPACK #-} !Double
                  {-# UNPACK #-} !Double
                  {-# UNPACK #-} !Double
-  deriving (Show)
+  deriving Show
 
 derivingUnbox "Vec3"
   [t| Vec3 -> (Double, Double, Double) |]
   [| \(Vec3 x y z) -> (x, y, z) |]
   [| \(x, y, z) -> Vec3 x y z |]
 
-vec3 :: Double -> Double -> Double -> Vec3
-vec3 = Vec3
-{-# INLINE vec3 #-}
-
 instance AdditiveGroup Vec3 where
-  (^+^) (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = vec3 (x1 + x2) (y1 + y2) (z1 + z2)
+  (^+^) (Vec3 x1 y1 z1) (Vec3 x2 y2 z2) = Vec3 (x1 + x2) (y1 + y2) (z1 + z2)
   {-# INLINE (^+^) #-}
-  zeroV = vec3 0 0 0
+  zeroV = Vec3 0 0 0
   {-# INLINE zeroV #-}
-  negateV (Vec3 x y z) = vec3 (-x) (-y) (-z)
+  negateV (Vec3 x y z) = Vec3 (-x) (-y) (-z)
   {-# INLINE negateV #-}
 
 instance VectorSpace Vec3 where
   type Scalar Vec3 = Double
-  (*^) c (Vec3 x y z) = vec3 (c * x) (c * y) (c * z)
+  (*^) c (Vec3 x y z) = Vec3 (c * x) (c * y) (c * z)
   {-# INLINE (*^) #-}
 
 instance InnerSpace Vec3 where
   (Vec3 x1 y1 z1) <.> (Vec3 x2 y2 z2) = x1 * x2 + y1 * y2 + z1 * z2
   {-# INLINE (<.>) #-}
-
-getX, getY, getZ :: Vec3 -> Double
-getX (Vec3 x _ _) = x
-getY (Vec3 _ y _) = y
-getZ (Vec3 _ _ z) = z
-{-# INLINE getX #-}
-{-# INLINE getY #-}
-{-# INLINE getZ #-}
-
-oneV :: Vec3
-oneV = vec3 1 1 1
-{-# INLINE oneV #-}
-
-vecMap :: (Double -> Double) -> Vec3 -> Vec3
-vecMap f (Vec3 x y z) = vec3 (f x) (f y) (f z)
-{-# INLINE vecMap #-}
 
