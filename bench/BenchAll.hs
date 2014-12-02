cd453f535fc9f0f5a87534a457f11128437f288e
Spenser Bauman <sabauma@gmail.com>
2014-12-01 22:38:57 -0500

    Actually add benchmark files

---
+++ bench/BenchAll.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import           Control.Concurrent (getNumCapabilities)
+import           Criterion
+import           Criterion.Main
+import           Criterion.Types
+import qualified Data.Vector.Unboxed as V
+import           MolecularDynamics.BHTree
+import           MolecularDynamics.System
+import           Text.Printf
+
+{-bhTreeCreation :: System -> BHTree-}
+{-bhTreeCreation = createBHTree-}
+
+bigCube :: System
+bigCube = makeCube 100
+
+midCube :: System
+midCube = makeCube 50
+
+smallCube :: System
+smallCube = makeCube 20
+
+stepSystem :: System -> System
+stepSystem = integrateSystem 0.00001
+
+main :: IO ()
+main = do
+  threads <- getNumCapabilities
+
+  let config = defaultConfig { csvFile   = Just $ printf "bench%0.3d.csv" threads
+                             , timeLimit = 10 }
+
+  defaultMainWith config
+    [ bgroup "tree-creation" [
+        bench "cube-50" $ nf createBHTree $ V.zip (positions midCube) (masses midCube),
+        bench "cube-100" $ nf createBHTree $ V.zip (positions bigCube) (masses bigCube)
+      ]
+    -- Weak head normal for is normal form for systems
+    , bgroup "cube-simulations" [
+        bench "cube-20" $ whnf stepSystem (makeCube 20),
+        bench "cube-30"  $ whnf stepSystem (makeCube 30),
+        bench "cube-40"  $ whnf stepSystem (makeCube 40)
+      ]
+    ]
+
